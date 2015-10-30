d9cc88740a8c745948d1b34f7334d4c882b4a98a
Charles Harrington <charles.harrington@gmail.com>
2015-10-29 20:54:27 -0700

    Mario baby now appears on screen

--- client/js/game.js
+++ client/js/game.js
@@ -3,18 +3,28 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer');
 var mainState = {
   preload: function () {
     game.stage.backgroundColor = '#03A9F4';
-    game.load.image('player', 'assets/yoshis.png'); 
+    game.load.image('player', 'assets/yoshis.png');
+    game.load.image('mario', 'assets/mario.png'); 
     game.load.image('ground', 'assets/ground.png');
   },
   create: function () {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    // Create the YOSHIS
     this.player = this.game.add.sprite(100, game.world.height-200, 'player');
     game.physics.arcade.enable(this.player);
     this.player.body.gravity.y = 1000; 
     //this.player.body.bounce.y = 0.5;
     this.player.body.collideWorldBounds = true;
     this.player.scale.setTo(0.5, 0.5);
+
+    // Create the baby mario
+    this.mario = this.game.add.sprite(100, 100, 'mario');
+    game.physics.arcade.enable(this.mario);
+    this.mario.body.gravity.y = 1000;
+    this.mario.body.collideWorldBounds = true;
+    this.mario.scale.setTo(0.5, 0.5);
+
 
     this.platforms = game.add.group();
     this.platforms.enableBody = true;
@@ -30,6 +40,7 @@ var mainState = {
   update: function () {
     for (var i = 0; i < this.ground.length; i++) {
       game.physics.arcade.collide(this.player, this.ground[i]);
+      game.physics.arcade.collide(this.mario, this.ground[i]);
     }
     this.player.body.velocity.x = 0;
 
